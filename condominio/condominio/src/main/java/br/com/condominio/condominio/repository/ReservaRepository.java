dda1f3d3b7aca57f607c7b5b66a466b28008cb8d
Gustavo Assalin <gustavo.assalin@hotmail.com>
2024-12-29 20:29:40 -0300

    Correcao

--- condominio/condominio/src/main/java/br/com/condominio/condominio/repository/ReservaRepository.java
+++ condominio/condominio/src/main/java/br/com/condominio/condominio/repository/ReservaRepository.java
@@ -1,9 +1,9 @@
 package br.com.condominio.condominio.repository;
 
-import java.awt.print.Pageable;
-import java.sql.Date;
+import java.time.LocalDate;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.condominio.condominio.model.Reserva;
@@ -11,15 +11,14 @@ import br.com.condominio.condominio.model.StatusReserva;
 
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
 
-	Page<Reserva> findByStatus(StatusReserva status);
-	Page<Reserva> findByDataReserva(Date dataReserva);
-	Page<Reserva> findByDataReservaBetween(Date startDate, Date endDate);
-	Page<Reserva> findByAreaComumId(Long areaComumId);
-	Page<Reserva> findByMoradorId(Long moradorId);
-	Page<Reserva> findByAreaComumIdAndStatus(Long areaComumId, StatusReserva status);
-	Page<Reserva> findByMoradorIdAndDataReserva(Long moradorId, Date dataReserva);
-	Page<Reserva> findByOrderByDataReservaAsc();
-	Page<Reserva> findByMoradorIdOrderByDataReservaDesc(Long moradorId);
 	Page<Reserva> findByStatus(StatusReserva status, Pageable pageable);
+	Page<Reserva> findByDataReserva(LocalDate dataReserva, Pageable pageable);
+	Page<Reserva> findByDataReservaBetween(LocalDate startLocalDate, LocalDate endLocalDate, Pageable pageable);
+	Page<Reserva> findByAreaComumId(Long areaComumId, Pageable pageable);
+	Page<Reserva> findByMoradorId(Long moradorId, Pageable pageable);
+	Page<Reserva> findByAreaComumIdAndStatus(Long areaComumId, StatusReserva status, Pageable pageable);
+	Page<Reserva> findByMoradorIdAndDataReserva(Long moradorId, LocalDate dataReserva, Pageable pageable);
+	Page<Reserva> findByOrderByDataReservaAsc();
+	Page<Reserva> findByMoradorIdOrderByDataReservaDesc(Long moradorId, Pageable pageable);
 
 }
